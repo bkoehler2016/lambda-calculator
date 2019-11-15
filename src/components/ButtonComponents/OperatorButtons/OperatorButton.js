06d13e61d89ec2c2ee9912a9a586e3971c3c8bd3
Ben Koehler <bkoehler85282@gmail.com>
2019-11-15 12:23:16 -0700

    got calculator working finally and looking good

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = props => {
   return (
-    <>
+    <button
+      className="operator-button"
+      onClick={() => props.addOperator(props.operatorText.value)}
+    >
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.operatorText.value}
+    </button>
   );
 };
+
 export default OperatorButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = props => {
   return (
-    <>
+    <button className="special-button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.specials}
+    </button>
   );
 };
 
